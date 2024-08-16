975b2bf76aa17eee3435ff4af6756fe02b20805b
Forever <forever@ForeverdeMacBook-Pro.local>
2024-08-16 17:58:58 +0800

    增删改测试

--- src/test/java/com/mashibing/mybatis/MybatisApplicationTests.java
+++ src/test/java/com/mashibing/mybatis/MybatisApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 @SpringBootTest
 class MybatisApplicationTests {
@@ -17,12 +18,12 @@ class MybatisApplicationTests {
     @Test
     void contextLoads() throws IOException {
 
-
+        //添加
         InputStream in = Resources.getResourceAsStream("mybatis-cfg.xml");
 
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
         SqlSession sqlSession = sessionFactory.openSession();
-        User user = new User("张明","山东","男");
+        User user = new User("张慧","湖州","女");
         int count = sqlSession.insert("com.mashibing.mybatis.pojo.User.addUser", user);
         System.out.println(count+":影响的行数");
         //mybatis默认不会进行数据的提交
@@ -30,4 +31,56 @@ class MybatisApplicationTests {
         sqlSession.close();
     }
 
+    @Test
+    void test() throws IOException {
+
+        //修改
+        InputStream in = Resources.getResourceAsStream("mybatis-cfg.xml");
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
+        SqlSession sqlSession = sessionFactory.openSession();
+        User user = new User(3,"张帆","湖北","男");
+        int count = sqlSession.insert("com.mashibing.mybatis.pojo.User.updateUser", user);
+        System.out.println(count+":影响的行数");
+        //mybatis默认不会进行数据的提交
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    void test2() throws IOException {
+        //根据id删除
+        InputStream in = Resources.getResourceAsStream("mybatis-cfg.xml");
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
+        SqlSession sqlSession = sessionFactory.openSession();
+        int count = sqlSession.delete("com.mashibing.mybatis.pojo.User.deleteUser", 2);
+        System.out.println(count+":影响的行数");
+        //mybatis默认不会进行数据的提交
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    void test03() throws IOException {
+        //查询所有
+        InputStream in = Resources.getResourceAsStream("mybatis-cfg.xml");
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
+        SqlSession sqlSession = sessionFactory.openSession();
+        List<Object> list = sqlSession.selectList("com.mashibing.mybatis.pojo.User.allUser");
+        System.out.println(list.toString());
+        //mybatis默认不会进行数据的提交
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    void test04() throws IOException {
+        //查询所有
+        InputStream in = Resources.getResourceAsStream("mybatis-cfg.xml");
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
+        SqlSession sqlSession = sessionFactory.openSession();
+        List<Object> list = sqlSession.selectList("com.mashibing.mybatis.pojo.User.queryById", 5);
+        System.out.println(list.toString());
+        //mybatis默认不会进行数据的提交
+        sqlSession.commit();
+        sqlSession.close();
+    }
 }
